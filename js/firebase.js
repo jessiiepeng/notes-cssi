415c4bb0530fab8eb7a0787cc1b5937a93b2f574
Glitch (day12-cssi-jp) <none>
2021-07-29 22:16:22 +0000

    🚄🎖 Updated with Glitch

---
+++ js/firebase.js
@@ -0,0 +1,16 @@
+console.log("js/firebase.js : Remember to replace the instructor's firebase keys with yours!")
+
+// TODO: Replace the following with your app's Firebase project configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyD7efxMKW8YfhnZG4ARHNP0iwOoxiFWYxM",
+    authDomain: "cssi-day-12-c9763.firebaseapp.com",
+    databaseURL: "https://cssi-day-12-c9763-default-rtdb.firebaseio.com",
+    projectId: "cssi-day-12-c9763",
+    storageBucket: "cssi-day-12-c9763.appspot.com",
+    messagingSenderId: "767142537327",
+    appId: "1:767142537327:web:dcfc0b614dcd89a1e2059a"
+  };
+// END TODO
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
